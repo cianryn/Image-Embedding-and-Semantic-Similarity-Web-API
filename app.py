75279cc900bbdb8c76e3fede3b355b39db10add6
cianryn <159548002+cianryn@users.noreply.github.com>
2024-04-05 19:24:23 +0000

    Adding test images

--- app.py
+++ app.py
@@ -7,50 +7,51 @@ import numpy as np
 import torch
 from torchvision.models import resnet18, ResNet18_Weights
 import torchvision.transforms as transforms
-from torch.autograd import Variable
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-## Initialization for Embedding Model
+## Initialize Embedding Model
 weights = ResNet18_Weights.DEFAULT
 model = resnet18(weights=weights)
 model.eval()
-layer = model._modules.get('avgpool')
-scaler = transforms.Resize((224, 224))
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+layer = model._modules.get('avgpool') # Layer can be changed to other fc layers
+scaler = transforms.Resize((224, 224)) # Imagenet input shape
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # Imagenet normalization params
 to_tensor = transforms.ToTensor()
 
 @app.post("/upload-images/")
 async def create_upload_files(files: List[UploadFile] = File(...)):
 
-    embeddings = {}
-    predictions = {}
+    embeddings = {} # Store embeddings
+    predictions = {} # Store image classification predictions
 
     for file in files:
         
-        # if file.content_type not in ["image/jpeg", "image/png"]:
-        #     return JSONResponse(status_code=400, content={"message": f"Invalid file type: {file.filename}"})
-
         try:
+
+            ## Image reading + pre-processing
             image_bytes = await file.read() 
             image_bytes = io.BytesIO(image_bytes)
             im = Image.open(image_bytes).convert('RGB')
             im = normalize(to_tensor(scaler(im))).unsqueeze(0) # Scale, to tensor, normalize + add batch dim
+
+            ## Perform embedding hook
             def hook_layer(module, inputs, output):
-                embeddings[file.filename] = output.data[0,:,0,0].detach().tolist()
+                embeddings[file.filename] = output.data[0,:,0,0].detach().numpy()
             hook = layer.register_forward_hook(hook_layer) ## Attach hook to avgpool layer
             with torch.no_grad():
                 out = model(im) # Perform a forward pass. The hook function will run automatically
-                class_id = out.argmax().item()
-                category_name = weights.meta["categories"][class_id]
-                predictions[file.filename] = category_name
             hook.remove() # detach hook when done
+
+            ## For image predictions (not necessary)
+            class_id = out.argmax().item()
+            category_name = weights.meta["categories"][class_id]
+            predictions[file.filename] = category_name
 
         except Exception as e:
             return JSONResponse(status_code=500, content={"message": f"Error processing {file.filename}: {str(e)}"})
 
-    # return JSONResponse(content={"embeddings": embeddings})
     return {"message": "You uploaded {} image(s)".format(len(files)), 
             "predictions": predictions,
             "embeddings": embeddings
--- similarity_analysis.py
+++ similarity_analysis.py
@@ -1,17 +1,60 @@
 import requests
 from glob import glob
+import argparse
+import numpy as np
+from tabulate import tabulate
 
+
+parser = argparse.ArgumentParser("Semantic similarity analysis.")
+parser.add_argument("--images_folder", default="images", help="path to all images")
+parser.add_argument("--ref_image", default="cat.png", help="The reference image name")
+args = parser.parse_args(args=[])
 
 url = 'http://0.0.0.0:8080/upload-images/' # The URL of the FastAPI endpoint 
-image_paths = glob("images/*") # Paths to the image files 
-files = [('files', (open(image_path, 'rb'))) for image_path in image_paths] # Prepare files `requests` format
+image_paths = glob(args.images_folder + "/*") # Paths to the image files 
+files = [('files', (open(image_path, 'rb'))) for image_path in image_paths] # Prepare files in requests format
 response = requests.post(url, files=files) # Send the POST request with the image files
-
-# Close files
 for _, file_tuple in files:
-    file_tuple.close()
+    file_tuple.close() # Close files
 
-# Print the response from the server
-print(response.json()["predictions"])
-for key, embedding in response.json()["embeddings"].items():
-    print(key, len(embedding))
+
+# Extract FastAPI responses
+response = response.json()
+api_message = response["message"]
+print(api_message)
+predictions = response["predictions"]
+embeddings = response["embeddings"]
+
+
+# Compute cosine similarity against reference image
+def cosine_similarity(v1, v2):
+    dot_product = np.dot(v1, v2)
+    norm_v1 = np.linalg.norm(v1)
+    norm_v2 = np.linalg.norm(v2)
+    return dot_product / (norm_v1 * norm_v2)
+
+ref_embedding = embeddings[args.ref_image]
+ref_embedding = np.array(ref_embedding)
+similarities = {}
+for key, embedding in embeddings.items():
+    if key != args.ref_image:
+        sim = cosine_similarity(ref_embedding, embedding)
+        similarities[key] = sim
+sorted_similarities = sorted(similarities.items(), key=lambda item: item[1])[::-1]
+
+
+## Display top 10 similar results
+sorted_similarities = sorted_similarities[0:10]
+headers = ["Reference Image", "Image Classification"]
+table = [(args.ref_image, predictions[args.ref_image])]
+print(tabulate(table, headers=headers, tablefmt="mixed_grid"))
+
+headers = ["Images", "Similarity to Reference", "Image Classification"]
+table = [(x[0],x[1],predictions[x[0]]) for x in sorted_similarities]
+print(tabulate(table, headers=headers, tablefmt="mixed_grid"))
+
+
+
+
+
+
